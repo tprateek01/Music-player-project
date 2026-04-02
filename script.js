4554674d1c9aa457eaea45ec28bf63d52f7c199c
Prateek Tripathi <prateekmrj08@gmail.com>
2026-04-03 03:53:46 +0530

    Enhanced Appearance and Some changes in title bar

--- script.js
+++ script.js
@@ -1,159 +1,130 @@
-console.log('Welcome to Music$');
-let audioElement= new Audio('1.mp3');
-//INTIALIZE song index
-let index=1;
-let playInfo=document.getElementById("playInfo");
-let masterPlay=document.getElementById("playbutton");
-let myprogress=document.getElementById("myProgress");
-let songGIF=document.getElementById('songGIF');
-let songItem=Array.from(document.getElementsByClassName("songItem"));
-let a=document.getElementsByClassName("songI");
-let showVolume=document.querySelector("#show-volume");
-let volumeIcon=document.querySelector("#volume-icon");
-let currentVolume=document.querySelector("#volume");
-let slider=document.querySelector(".volume");
+let songIndex = 0;
+let audioElement = new Audio();
+let masterPlay = document.getElementById("playbutton");
+let myProgress = document.getElementById("myProgress");
+let songGIF = document.getElementById('songGIF');
+let playInfo = document.getElementById("playInfo");
+let songItems = Array.from(document.getElementsByClassName("songItem"));
 
-volumeIcon.addEventListener("click",muteSound);
-currentVolume.addEventListener("change",changeVolume);
-slider.addEventListener("change",changeDuration);
-//Indexed the list of song
-var song=[
-    {songName:"Ishq- Faheem Abdulah,Rauhan Malik" ,filePath:"1.mp3", coverPath:"1.jpg"},
-    {songName:"Tu Hain To Mai Hoon - Arijit Singh,Afshana" ,filePath:"2.mp3", coverPath:"2.jpg"},
-    {songName:"Raanjhana Ve - Antara Mitra,Soham Naik" ,filePath:"3.mp3", coverPath:"3.jpg"},
-    {songName:"Filhaal - B Praak" ,filePath:"4.mp3", coverPath:"4.jpg"},
-    {songName:"Tera Zikr - Darshan Raval" ,filePath:"5.mp3", coverPath:"5.jpg"},];
+let currentVolume = document.querySelector("#volume");
+let volumeIcon = document.querySelector("#volume-icon");
+let showVolume = document.querySelector("#show-volume");
 
-//update songList with songs array
-songItem.forEach((element,i) => {
-    element.getElementsByTagName("img")[0].src=song[i].coverPath;
-    element.getElementsByClassName("songName")[0].innerText=song[i].songName;
+let songs = [
+    {songName: "Ishq - Faheem Abdullah", filePath: "1.mp3", coverPath: "1.jpg"},
+    {songName: "Tu Hain To Mai Hoon", filePath: "2.mp3", coverPath: "2.jpg"},
+    {songName: "Raanjhana Ve", filePath: "3.mp3", coverPath: "3.jpg"},
+    {songName: "Filhaal - B Praak", filePath: "4.mp3", coverPath: "4.jpg"},
+    {songName: "Tera Zikr - Darshan Raval", filePath: "5.mp3", coverPath: "5.jpg"},
+];
 
-    
+const formatTime = (time) => {
+    if (isNaN(time)) return "00:00";
+    let min = Math.floor(time / 60);
+    let sec = Math.floor(time % 60);
+    return `${min}:${sec < 10 ? '0' + sec : sec}`;
+};
+
+const syncUI = () => {
+    if (audioElement.paused) {
+        masterPlay.classList.replace("fa-circle-pause", "fa-circle-play");
+        songGIF.style.opacity = 0;
+        Array.from(document.getElementsByClassName('songI')).forEach(el => el.classList.replace("fa-circle-pause", "fa-circle-play"));
+    } else {
+        masterPlay.classList.replace("fa-circle-play", "fa-circle-pause");
+        songGIF.style.opacity = 1;
+        Array.from(document.getElementsByClassName('songI')).forEach(el => el.classList.replace("fa-circle-pause", "fa-circle-play"));
+        let currentIcon = document.getElementById(`${songIndex}`);
+        if(currentIcon) currentIcon.classList.replace("fa-circle-play", "fa-circle-pause");
+        playInfo.innerText = `${songs[songIndex].songName}`;
+    }
+};
+
+// Initialization
+songs.forEach((element, i) => {
+    if(songItems[i]) {
+        songItems[i].getElementsByTagName("img")[0].src = songs[i].coverPath;
+        songItems[i].getElementsByClassName("songName")[0].innerText = songs[i].songName;
+        let tAudio = new Audio(songs[i].filePath);
+        tAudio.addEventListener('loadedmetadata', () => {
+            songItems[i].querySelector(".duration-display").innerText = formatTime(tAudio.duration);
+        });
+    }
 });
 
+// Initial Volume (Sync with Seekbar/LocalStorage)
+const savedVol = localStorage.getItem('musicVol') || 100;
+audioElement.volume = savedVol / 100;
+currentVolume.value = savedVol;
+showVolume.innerText = savedVol;
 
-masterPlay.addEventListener('click',()=>
-{ //to play the song
-    if(audioElement.paused|| audioElement.currentTime<=0) {
+masterPlay.addEventListener('click', () => {
+    if (audioElement.paused || audioElement.currentTime <= 0) {
+        if(!audioElement.src) audioElement.src = songs[songIndex].filePath;
         audioElement.play();
-        masterPlay.classList.remove("fa-play-circle");
-        masterPlay.classList.add("fa-pause-circle");
-        songGIF.style.opacity=1; //to make the GIF visible while playing
-
-
-}
-//to pause the song
-else{
-    audioElement.pause();
-    masterPlay.classList.remove("fa-pause-circle");
-    masterPlay.classList.add("fa-play-circle");
-    songGIF.style.opacity=0;  //to make the GIF invisible while playing
-
-}
-
-})
-//seek the progressbar
-audioElement.addEventListener('timeupdate',()=>{
-    progress=parseInt((audioElement.currentTime/audioElement.duration)*100);
-    myprogress.value=progress;
-
-})
-// it will allow us to change the song progress time
-myprogress.addEventListener('change',()=>{
-    audioElement.currentTime=myprogress.value * audioElement.duration/100;
-})
-
-    let makeplay=()=>{
-    Array.from(document.getElementsByClassName("songI")).forEach((element)=>{
-        element.classList.remove("fa-pause-circle");
-        element.classList.add("fa-play-circle");})
-
-}
-
-Array.from(document.getElementsByClassName("songI")).forEach((element)=>{
-    element.addEventListener('click',(e)=>{
-        console.log(e.target.id);
-        makeplay();
-         index=parseInt(e.target.id);
-         playInfo.innerText=song[index-1].songName;
-        e.target.classList.remove("fa-play-circle");
-        e.target.classList.add("fa-pause-circle");
-        masterPlay.classList.remove("fa-play-circle");
-        masterPlay.classList.add("fa-pause-circle");
-        audioElement.src=index+'.mp3';
-        audioElement.currentTime=0;
-        audioElement.play();
-        songGIF.style.opacity=1;
-        masterPlay.classList.remove("fa-play-circle");
-        masterPlay.classList.add("fa-pause-circle");
-      
-        
-    })
-})
-document.getElementById("previous").addEventListener('click',()=>
-{   if(index>0){
-        index-=1;
-        makeplay();
-    
+    } else {
+        audioElement.pause();
     }
-    else{
-        index=1;
-    }
-       //this logic to transfer the pause icon to the previous song which will be played
-        makeplay();
-        audioElement.src=index+'.mp3';
-        playInfo.innerText=song[index-1].songName;
-        audioElement.currentTime=0;
-        audioElement.play();
-        let x=document.getElementById(index);
-        x.classList.remove("fa-play-circle");
-        x.classList.add("fa-pause-circle");
-    })
-document.getElementById("next").addEventListener('click',()=>
-    {   if(index<6){
-            index+=1;
-            }
-        
-        else{
-            index=1;
-        }
-            makeplay();
-            audioElement.src=index+'.mp3';
-            playInfo.innerText=song[index-1].songName;
-            audioElement.currentTime=0;
+    syncUI();
+});
+
+Array.from(document.getElementsByClassName("songI")).forEach((element) => {
+    element.addEventListener('click', (e) => {
+        let clickedId = parseInt(e.target.id);
+        if(songIndex === clickedId && !audioElement.paused) {
+            audioElement.pause();
+        } else {
+            songIndex = clickedId;
+            audioElement.src = songs[songIndex].filePath;
             audioElement.play();
-            let x=document.getElementById(index);
-            x.classList.remove("fa-play-circle");
-            x.classList.add("fa-pause-circle");
-            
-    })
-    function muteSound(){
-        audioElement.volume=0;
-        showVolume.innerHTML=0;
-        currentVolume.value=0;
-        volumeIcon.classList.remove("fa-volume-up");
-        volumeIcon.classList.add("fa-volume-mute");
+        }
+        syncUI();
+    });
+});
+
+document.getElementById("next").addEventListener('click', () => {
+    songIndex = (songIndex >= songs.length - 1) ? 0 : songIndex + 1;
+    audioElement.src = songs[songIndex].filePath;
+    audioElement.play();
+    syncUI();
+});
+
+document.getElementById("previous").addEventListener('click', () => {
+    songIndex = (songIndex <= 0) ? songs.length - 1 : songIndex - 1;
+    audioElement.src = songs[songIndex].filePath;
+    audioElement.play();
+    syncUI();
+});
+
+audioElement.addEventListener('timeupdate', () => {
+    myProgress.value = parseInt((audioElement.currentTime / audioElement.duration) * 100) || 0;
+    if(!audioElement.paused) {
+        playInfo.innerText = `${songs[songIndex].songName} (${formatTime(audioElement.currentTime)} / ${formatTime(audioElement.duration)})`;
     }
-   function changeVolume(){
-    showVolume.innerHTML=currentVolume.value;
-    audioElement.volume=currentVolume.value/100;
-    volumeIcon.classList.remove("fa-volume-mute");
-    volumeIcon.classList.add("fa-volume-up");
-    if (currentVolume.value==0){
-        volumeIcon.classList.remove("fa-volume-up");
-        volumeIcon.classList.add("fa-volume-mute");
+});
 
-    }
+myProgress.addEventListener('input', () => {
+    audioElement.currentTime = (myProgress.value * audioElement.duration) / 100;
+});
 
-   }
-   function changeDuration(){
-    let sliderPosition=audioElement.duration*(slider.value/100);
-    audioElement.currentTime=sliderPosition;
-   
+currentVolume.addEventListener('input', (e) => {
+    let vol = e.target.value;
+    audioElement.volume = vol / 100;
+    showVolume.innerText = vol;
+    localStorage.setItem('musicVol', vol);
+    volumeIcon.className = vol == 0 ? "fas fa-volume-mute" : "fas fa-volume-up";
+});
 
-   }
+document.getElementById('theme-toggle').addEventListener('click', () => {
+    const isDark = document.documentElement.getAttribute('data-theme') === 'dark';
+    document.documentElement.setAttribute('data-theme', isDark ? 'light' : 'dark');
+});
 
+document.getElementById('searchBar').addEventListener('input', (e) => {
+    let q = e.target.value.toLowerCase();
+    songItems.forEach((item, i) => {
+        if(songs[i]) item.style.display = songs[i].songName.toLowerCase().includes(q) ? "flex" : "none";
+    });
+});
 
-    
-
+audioElement.addEventListener('ended', () => document.getElementById("next").click());
